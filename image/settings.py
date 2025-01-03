49038a2ae6fb085036ef72a9ac98e7d05322ae06
Sudharshan <sudharshan1.bit@gmail.com>
2025-01-04 03:52:25 +0530

    3

--- image/settings.py
+++ image/settings.py
@@ -75,14 +75,23 @@ WSGI_APPLICATION = 'image.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'image',
+#         'USER':'Captain',
+#         'PASSWORD':'Captain',
+# }}
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'image',
-        'USER':'Captain',
-        'PASSWORD':'Captain',
-}}
-
+        'NAME': os.getenv('DB_NAME', 'default_db_name'),
+        'USER': os.getenv('DB_USER', 'default_user'),
+        'PASSWORD': os.getenv('DB_PASSWORD', 'default_password'),
+        'HOST': os.getenv('DB_HOST', 'localhost'),
+        'PORT': os.getenv('DB_PORT', '5432'),
+    }
+}
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
 
